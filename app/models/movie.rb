933c5e904e66380dba4601d5aa039145c8cb0a21
Justin Park <justinjspark@berkeley.edu>
2021-10-20 05:03:20 +0000

    part 1 trial

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,7 +8,7 @@ class Movie < ActiveRecord::Base
   # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
   #  movies with those ratings
   # if ratings_list is nil, retrieve ALL movies
-    if !ratings_list.nil?
+    if ratings_list.length > 0
       Movie.where(rating: ratings_list.map { |rating| rating.upcase })
     else
       Movie.all
